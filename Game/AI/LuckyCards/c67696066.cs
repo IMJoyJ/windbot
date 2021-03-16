2acffeb89d5c05ed1fc6fbe90e3b6ab371ec0fa0
Joy J <316351048@qq.com>
2021-03-16 20:44:16 +0800

    add card name

--- Game/AI/LuckyCards/c67696066.cs
+++ Game/AI/LuckyCards/c67696066.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace WindBot.Game.AI.LuckyCards
 {
+    /// <summary>
+    /// Emトリック・クラウン / Performage Trick Clown / 娱乐法师 戏法小丑
+    /// </summary>
     public class C67696066 : LuckyCard
     {
         public override bool ShouldExec(Duel duel, ClientField bot, ClientField enemy, ClientCard card)
